51473bb567791ddad318395d22e867774aae1274
Mack McCall <mackmc9924@gmail.com>
2023-08-15 08:28:50 -0600

    FizzBuzz program created

---
+++ FizzBuzz/Fizzing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Fizzing
+    {
+        public static string FizzBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "fizzbuzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "fizz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "buzz";
+            }
+            else
+                return ($"The number {number} is not divisible by 3 or 5.");
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿using FizzBuzz;
+
+while (true)
+{
+    Console.Write("What number would you like to enter into the FizzBuzz?: ");
+    if (!int.TryParse(Console.ReadLine(), out int number))
+    {
+        Console.WriteLine("Invalid Input, Try Again");
+    }
+    else
+        Console.WriteLine(Fizzing.FizzBuzz(number));
+        return;
+}
